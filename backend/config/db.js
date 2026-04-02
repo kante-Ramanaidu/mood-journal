d1867f67d5b28ae7e7b449fd17744f1da52d21de
Ram kante <ramkante84@gmail.com>
2026-04-02 12:30:28 +0530

    fix db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,6 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 
-
-const { Pool } = require('pg');
-require('dotenv').config();
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false } // ← required for Supabase
